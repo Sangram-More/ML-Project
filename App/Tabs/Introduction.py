5a5066b0607b1a5590bb2832b25a443fa21b30cf
Sangram More <sangrammoreus@gmail.com>
2025-01-30 02:09:09 -0700

    Added new animations to Animations folder. Added 6 new sections to Introduction tab.

--- App/Tabs/Introduction.py
+++ App/Tabs/Introduction.py
@@ -4,6 +4,22 @@ import json
 import os
 
 
+#--------------- Custom CSS Styling -----------------------------
+
+# Custom CSS for text justification
+st.markdown(
+    """
+    <style>
+    .justified-text {
+        text-align: justify;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# -----------------------------------------------------------------------------
+
 # Url for downloading animations: https://lottiefiles.com/free-animations/free
 
 # There are 2 ways to add animations using streamlit_lottie package:
@@ -56,16 +72,57 @@ import os
 
 # Implementing animation function
 def animation_file(filepath: str):
-    with open(filepath, "r") as f:
+    with open(filepath, "rb") as f:
         return json.load(f)
 
 def Bank_Animation():   
     animation = st_lottie(
-        animation_file("App\Tabs\Animations\Bank.json"),
+        animation_file(r"App\Tabs\Animations\Bank.json"),
         speed = 1,
         reverse = False,
         loop = True,
-        quality = "medium"
+        quality = "high"
+    )
+    return animation
+
+def Thinking_Animation():   
+    animation = st_lottie(
+        animation_file(r"App\Tabs\Animations\Thinking.json"),
+        speed = 1,
+        reverse = False,
+        loop = True,
+        quality = "high"
+    )
+    return animation
+
+def UpTrend_Animation():   
+    animation = st_lottie(
+        animation_file(r"App\Tabs\Animations\UpTrend.json"),
+        speed = 1,
+        reverse = False,
+        loop = True,
+        quality = "high"
+    )
+    return animation
+
+def DownTrend_Animation():   
+    animation = st_lottie(
+        animation_file(r"App\Tabs\Animations\DownTrend.json"),
+        speed = 1,
+        reverse = False,
+        loop = True,
+        quality = "high"
+    )
+    return animation
+
+def Speedometer_Animation():   
+    animation = st_lottie(
+        animation_file(r"App\Tabs\Animations\Speedometer.json"),
+        speed = 1,
+        reverse = False,
+        loop = True,
+        height=400, 
+        width=400
     )
     return animation
 
@@ -76,13 +133,94 @@ def Bank_Animation():
 st.title("What is this all about?")
   
 # -----------------------------------------------------------
+# Section 1
 
 # Creating 2 columns to add animation and text side by side.
 
-column1, column2 = st.columns(2, gap="large", vertical_alignment="center")
+column1_1, column2_1 = st.columns(2, gap="large", vertical_alignment="center")
 
-with column1:
+with column1_1:
     Bank_Animation()
 
-with column2:
+with column2_1:
     st.header("What do you know about the term *US Fedral Rates?*")
+    st.markdown('<p class="justified-text">Banks lend money to each other overnight to meet daily cash needs. The Fed sets a target rate for these loans, which influences all interest rates in the economy.</p>', unsafe_allow_html=True)
+    
+st.divider()
+
+# -----------------------------------------------------------
+# Section 2
+
+# Creating 2 columns to add animation and text side by side.
+
+column1_2, column2_2 = st.columns(2, gap="large", vertical_alignment="center")
+
+with column1_2:
+    Thinking_Animation()
+
+with column2_2:
+    st.header("Why does it matter?")
+    st.markdown('<p class="justified-text">Fed Rate affects everything from credit card rates, home loans, business loans, to how much interest you earn on your savings.</p>', unsafe_allow_html=True)
+
+# -----------------------------------------------------------
+# Section 3
+
+# Creating 2 columns to add animation and text side by side.
+
+column1_3, column2_3 = st.columns(2, gap="large", vertical_alignment="center")
+
+with column1_3:
+    st.subheader("How does it impact you?")
+    st.markdown("""
+    #### **When rates go UP:**
+    - Borrowing becomes expensive (higher loan & credit card rates).
+    - Mortgage and car loan payments increase.
+    - Saving money is more rewarding (higher bank interest rates).
+    - Economy slows down to control inflation (rising prices).
+    """)
+
+with column2_3:
+    UpTrend_Animation()
+
+# -----------------------------------------------------------
+# Section 4
+
+# Creating 2 columns to add animation and text side by side.
+
+column1_4, column2_4 = st.columns(2, gap="large", vertical_alignment="center")
+
+with column1_4:
+    st.markdown("""
+    #### **When rates go DOWN:**
+    - Borrowing becomes cheaper (lower loan & credit card rates).
+    - People & businesses spend more.
+    - Economy speeds up, helping job growth.
+    """)
+
+with column2_4:
+    DownTrend_Animation()
+
+st.divider()
+
+# -----------------------------------------------------------
+# Section 5
+
+st.subheader("Why does the Fed change it?")
+st.markdown("""
+    - To fight inflation, they raise rates to slow down spending.
+    - To boost the economy, they cut rates to encourage borrowing and investing.
+    """)
+
+st.divider()
+
+# -----------------------------------------------------------
+# Section 5
+
+st.markdown('<h2 style="text-align: justify;"> Think of it like adjusting the speed of a car: the Fed uses interest rates to speed up or slow down the economy as needed!</h2>', unsafe_allow_html=True)
+
+block1, block2, block3 = st.columns([2, 6, 1])
+
+with block2:
+    Speedometer_Animation()
+
+st.divider()
